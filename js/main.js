5a4059acc660d887084caeba6d83435181d7e08d
janetmndez <janetmendez.03@gmail.com>
2019-03-02 17:49:36 -0500

    added remaining functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('.bookinput');
 const searchButton = document.querySelector('.booksearch');
 const bookDisplay = document.querySelector('.display');
 
 function display(){
     let searchKey = searchInput.value;
-    console.log(searchKey);
 
     if(searchKey !== 0){
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchKey}&maxResults=14`)
@@ -18,34 +18,41 @@ function display(){
 }
 
 function logBooks(arr){
-    const html = arr.map(b => {
-        console.dir(b);
-        const title = b.volumeInfo.title;
-        const image = (b.volumeInfo.hasOwnProperty('imageLinks')) ? (b.volumeInfo.imageLinks.thumbnail) : "https://placekitten.com/200/350";
-        const authors = (b.volumeInfo.hasOwnProperty('authors')) ? (b.volumeInfo.authors.join(', ')) : "N/A";
-        const publisher = (b.volumeInfo.hasOwnProperty('publisher')) ? (b.volumeInfo.publisher) : "N/A";
-
-        return `
-        <div class="book__hold">
-            <img class="book__image"src="${image}">
-            <div class="book__info">
+    if (arr !== undefined){
+        const html = arr.map(b => {
+            console.dir(b);
+            const title = b.volumeInfo.title;
+            const image = (b.volumeInfo.hasOwnProperty('imageLinks')) ? (b.volumeInfo.imageLinks.thumbnail) : "https://placekitten.com/200/350";
+            const authors = (b.volumeInfo.hasOwnProperty('authors')) ? (b.volumeInfo.authors.join(', ')) : "N/A";
+            const publisher = (b.volumeInfo.hasOwnProperty('publisher')) ? (b.volumeInfo.publisher) : "N/A";
+            const infoLink = b.volumeInfo.infoLink;
+            return `
+            <div class="book__hold">
                 <h1>${title}</h1>
-                <p>By ${authors}</p>
-                <p>Published by ${publisher}</p>
+                <div class="book__info">
+                    <img class="book__image"src="${image}">
+                    <div class="book__details">
+                        <p class="detail">Author(s):</p>
+                        <p>${authors}</p>
+                        <p class="detail">Publisher</p>
+                        <p>${publisher}</p>
+                    </div>
+                </div>
+                <div class="book__button"><a href="${infoLink}" target="__blank" class="book__link">More Information</a></div>
+                
             </div>
-            
-        </div>
-        `
-    }).join('');
-
-    bookDisplay.innerHTML = html;
+            `
+            }).join('');
+        return bookDisplay.innerHTML = html;
+    }
+    return bookDisplay.innerHTML = `<p class="placement-text">No results were found.</p>`;
 }
 
 function isEnter(e){
     if(e.keyCode == 13){
-        console.log("yes");
         display();
     }
 }
 
 searchButton.addEventListener('click', display);
+searchInput.addEventListener('keydown', (e) => isEnter(e));
